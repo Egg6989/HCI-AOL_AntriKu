c1c00fb6edb0ffe85f21fd08fe9a7d7a3dad0e8f
Keanan Enrico <enricokeanan@gmail.com>
2026-06-09 11:49:14 +0700

    perubahan js dikit

--- home_mobile_antriku/js/detail-layanan.js
+++ home_mobile_antriku/js/detail-layanan.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <span style="font-weight: 700; font-size: 16px; color: rgb(132, 86, 0);">${layanan.harga}</span>
                 <div style="display: flex; align-items: center; gap: 12px;">
                     <span style="font-size: 14px; font-weight: 500; color: var(--hex-text-main);">Pilih</span>
-                    <div style="width: 32px; height: 32px; background-color: var(--hex-primary-dark); color: var(--hex-bg-white); border-radius: 50%; display: flex; align-items: center; justify-content: center;">
+                    <div style="width: 32px; height: 32px; ba ckground-color: var(--hex-primary-dark); color: var(--hex-bg-white); border-radius: 50%; display: flex; align-items: center; justify-content: center;">
                         <i class="fa-solid fa-chevron-right" style="font-size: 12px;"></i>
                     </div>
                 </div>
